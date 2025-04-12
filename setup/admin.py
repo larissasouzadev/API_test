6813833e06418d2adb07a476781a59a9d64fbc88
larissa <157990199+Larissa3636@users.noreply.github.com>
2025-04-12 19:45:02 -0300

    second change

--- setup/admin.py
+++ setup/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from setup import models 
+from setup.models import Client, Category, Sales
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Sales)
--- setup/models.py
+++ setup/models.py
@@ -1,27 +1,34 @@
 from django.db import models
-
+from django.utils.timezone import now
 
 class Client(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_lenght=50, default='')
-    social_nature = models.CharField(max_leght=100, default='')
-    phone = models.CharField(max_lenght=11, default='')
-    revenues = models.FloatField(max_digits = 8, default=0.00)
-    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50, default='')
+    social_nature = models.CharField(max_length=100, default='')
+    phone = models.CharField(max_length=11, default='')
+    revenues = models.FloatField(blank=True, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+        
     def __str__(self):
         return self.name
     # class category
 class Category(models.Model):
-    name = models.CharField(max_leght=100, default='')
+    name = models.CharField(max_length=100, default='')
     def __str__(self):
         return self.name
 class Sales(models.Model): 
+     client = models.ForeignKey(Client, on_delete=models.CASCADE, default='')
      category = models.ForeignKey(Category,  on_delete=models.CASCADE)
      id_sale = models.AutoField(primary_key=True)  
-     product = models.CharField(max_lenght=50,default='' )
-     supplier = models.CharField(max_lenght=100, default='')
-     price_sale = models.FloatField(max_digits=8, default=0.00)
-     data = models.DateTimeField(auto_now=True)
+     product = models.CharField(max_length=50,default='' )
+     supplier = models.CharField(max_length=100, default='')
+     price_sale = models.FloatField(blank=False, default=0.00)
+     created_at = models.DateTimeField(auto_now_add=True,null=True)
+     updated_at = models.DateTimeField(auto_now=True)
+     class Meta:
+        verbose_name_plural:'Sales'
      def __str__(self):
          return self.product
+     
