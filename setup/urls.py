50f71141689881e02bfc22f60e09b398a2213fb9
larissa <157990199+Larissa3636@users.noreply.github.com>
2025-04-16 11:03:33 -0300

    commiting api rest ful

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from setup import views 
-from setup.views import ClientAPIView
+from setup.views import  ClientAPIView
 urlpatterns = [
-    path('client_get/',ClientAPIView.as_view(), name='client_get/' )
+    path('client_get/', ClientAPIView.as_view(), name='client_get/' ),
+    path('client_post/',ClientAPIView.as_view(), name='client_post/'),
 ]
--- setup/views.py
+++ setup/views.py
@@ -9,29 +9,30 @@ from setup import models
 from setup.models import Client, Category, Sales
 from rest_framework.response import Response
 from rest_framework.views import APIView
-# Create your views here.
+from rest_framework.permissions import AllowAny
 
-
-# class APIVIEW using the CBV method
+# it may process one by one get(client)
 class ClientAPIView(APIView):
-    # it may process one by one get(client)
-    def get(self, request): 
-        clients = Client.objects.all()
-        serializer = ClientSerializer(clients, many=True)
-        return Response(status=status.HTTP_200_OK)
-
+    permission_classes = [AllowAny]
+    def get( self,request): 
+        if request.method =="GET":
+            queryset = Client.objects.all()
+            serializer = ClientSerializers(queryset, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     # it may process one by one post(client)
+
     def post(self, request):
-        serializer = ClientSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        if request.method =="POST":
+             serializer = ClientSerializers(data=request.data)
+             if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
         
-# # it may process one by one get(category)
-class CategoryAPIView(APIView):
-    pass
+                
+# it may process one by one get(category)
+
  # it may process one by one post(category)
   # it may process one by one get(Sales)
    # it may process one by one post(sales)
